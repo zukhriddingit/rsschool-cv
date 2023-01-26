18661a367b4a45e3f7b71c33cab9499eae1b3dd0
Zukhriddin Fakhriddinov <faxriddinovzuxriddin60@gmail.com>
2023-01-26 16:24:34 +0500

    feat: finished 380px

--- plants/index.js
+++ plants/index.js
@@ -1,1 +1,18 @@
-console.log("Верстка валидная - нету варнингов или ерроров (+10) \n Верстка семантическая - присутствуют header, main, footer, 5 sections, 1 h1, 4 h2, 1 nav, 2 ul>li>a, 5 buttons (+20) \n Верстка соответствует макету - присутствуют header, sections welcome, about, service, prices, contacts, и footer (+48), требования к css выполнены - использованы display: flex, margin: 0 auto, фоновый цвет тянется на всю ширину страницы, иконки добавлены в формате .svg, изображения добавлены в формате .jpg, есть favicon (+12) \n Интерактивность, реализуемая через css - плавная прокрутка, ссылки кликабельны и ведут к гитхабу и к сайту школы, есть cursor: pointer и transitions при hoverе. (+15) \n Итого: 105 баллов")
+const nav_burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation');
+const body = document.querySelector('body');
+const navLinks = document.querySelectorAll('a');
+
+nav_burger.addEventListener('click', () => {
+    body.classList.toggle('stop-scroll');
+    nav_burger.classList.toggle('burger--active');
+    navigation.classList.toggle('nav--visible');
+});
+
+navLinks.forEach(el => {
+    el.addEventListener('click', () =>{
+        body.classList.remove('stop-scroll');
+        nav_burger.classList.remove('burger--active');
+        navigation.classList.remove('nav--visible');
+    })
+})
